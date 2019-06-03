a948a696765461fb338484d15e3b6d77d5dcd0ec
Tommy <tommy.wong.tbs@gmail.com>
2019-06-03 12:13:50 +0800

    fix usb vbus

--- radio/src/targets/common/arm/stm32/usb_bsp.c
+++ radio/src/targets/common/arm/stm32/usb_bsp.c
@@ -54,8 +54,8 @@ void USB_OTG_BSP_Init(USB_OTG_CORE_HANDLE *pdev)
   GPIO_InitStructure.GPIO_Pin = USB_GPIO_PIN_VBUS;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN ;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
   GPIO_Init(USB_GPIO, &GPIO_InitStructure);
   
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
